76080b81634974ba981ff5274612b87efbfa591a
John Meehan <john.meehan1@gmail.com>
2012-10-03 17:05:43 -0700

    tried q1

--- part1.rb
+++ part1.rb
@@ -1,7 +1,20 @@
-def palindrome?(str)
+# Determine wheter a given string is a palindrome (reads the same backwards)
+# ignore case punctuation and nonword characters
+def palindrome?(str ="kayak")
   # YOUR CODE HERE
+	if str =~ /\A(?<p>| \w | (?: (?<l>\w) \g<p> \k<l+0> ))\z/x 
+	#str = str.downcase
+	#if str == str.reverse
+	#	return true
+	
 end
 
-def count_words(str)
+#def count_words(str)
   # YOUR CODE HERE
-end
+#end
+
+
+puts palindrome()
+
+
+
