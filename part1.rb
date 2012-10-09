fb27b594de75f5da478dd9bf5091780caf135c55
John Meehan <john.meehan1@gmail.com>
2012-10-08 18:32:10 -0700

    Finished Q1 100%

--- part1.rb
+++ part1.rb
@@ -1,23 +1,47 @@
-# Determine wheter a given string is a palindrome (reads the same backwards)
-# ignore case punctuation and nonword characters
-def palindrome?(str ="kayak")
-  # YOUR CODE HERE
-  if str =~ /\A(?<p>| \w | (?: (?<l>\w) \g<p> \k<l+0> ))\z/x
-  #str = str.downcase
-  #if str == str.reverse
-  # return true
-
-
-    john meehan wrote this 8/10/12 21.42
-
+def palindrome?(str)
+	# YOUR CODE HERE
+	# Determine wheter a given string is a palindrome (reads the same backwards)
+	# ignore case punctuation and nonword characters	
+	#p = str.downcase.gsub("/[[\W*\d*\s*]*]/x", "")
+	p = str.downcase.gsub(/[^a-z]*\s*\d*/, "")
+	#puts str
+	#puts p
+	#puts p.reverse
+	p == p.reverse
 end
 
-#def count_words(str)
+
+
+def count_words(str)
   # YOUR CODE HERE
-#end
+	wordhash = Hash.new
+	#puts wordhash.empty? #test
+		
+	# strip string of none word chars
+	
+	str.downcase.scan(/\b\w*\b/) do |word|
+		if wordhash.has_key?(word) 
+			wordhash[word] += 1
+		elsif word == ""
+		else wordhash[word] = 1
+		end
+	
+		# if word is in the hash increment
+		# else add the word to the hash and make it 1
+	end
+	p wordhash
+end
 
+puts "Test Cases *******************1  ******************"
+#Test Cases
+puts palindrome?("A man, a plan, a canal -- Panama")  # => true
+puts palindrome?("Madam, I'm Adam!")                  # => true
+puts palindrome?("Abracadabra")                       # => false (nil is also ok)
 
-puts palindrome()
-
-
+puts "Test Cases **********************2 ****************"
+#Test Cases
+puts count_words("A man, a plan, a canal -- Panama")
+puts count_words "Doo bee doo bee doo"
+puts count_words "blah blah blah blah blah"
+    
 
