6ce740f86cf0b91aa1862bef5f82b08718d29604
John Meehan <john.meehan1@gmail.com>
2012-10-09 15:35:24 -0700

    Finish Q3 100%

--- part3.rb
+++ part3.rb
@@ -1,3 +1,30 @@
 def combine_anagrams(words)
  # YOUR CODE HERE
+	anagramarray = Array.new # an array for the results
+	
+	#words = words.uniq # removes duplicate words	
+
+	words.each do |aword|  #do for allthe words
+			temp = [] #holds result
+		words.each do |nextword|	
+			if (aword.downcase.split(//).sort == nextword.downcase.split(//).sort) #compare characters
+				temp.push(nextword)# if the same MOVE IT to the new array
+			end
+		end
+		anagramarray.push(temp) # push temp into results array 
+	end
+		
+	# ouput the arrays!!
+	return anagramarray.uniq
+
 end
+
+#test it
+puts combine_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
+
+# input: ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] 
+# output: [ ["cars", "racs", "scar"],
+#           ["four"],
+#           ["for"],
+#           ["potatoes"],
+#           ["creams", "scream"] ]
